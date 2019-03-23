99e8417f861def367a0542f6d49307e1d62981d9
木村　幸介 <kosukekimura0528@gmail.com>
2019-03-23 10:24:53 +0900

    ルートディレクトリの追加、validatioの追加

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,10 @@
 class Property < ApplicationRecord
+  validates :name,presence: true
+  validates :rent,presence: true
+  validates :address,presence: true
+  validates :age,presence: true 
+  validates :remark, presence: true
+
   has_many :stations, inverse_of: :property
   accepts_nested_attributes_for :stations, allow_destroy: true
 end
